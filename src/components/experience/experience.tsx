b78110444760bc16c1fbafb8c42a64afd7f2dd05
itaLoopCode <italo_j.s@hotmail.com>
2024-06-11 18:34:58 -0300

    commited

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,5 +1,6 @@
 import "./experience.scss";
 
+import Image from "next/image";
 import { SectionTitle } from "../SectionTitle/sectionTitle";
 
 export function Experience() {
@@ -12,8 +13,58 @@ export function Experience() {
           clientes
         </p>
       </div>
-      <div className="experience-time"></div>
-
+      <div className="experience-time">
+        <div className="experience-language">
+          <Image
+            src="/react.png"
+            alt="React Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure m2"><span>2 anos</span></div>
+          </div>
+        </div>
+        <div className="experience-language">
+          <Image
+            src="/js.png"
+            alt="Javascript Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <div className="experience-unit">
+            <div className="experience-measure m3"><span>3 anos</span></div>
+          </div>
+        </div>
+        <div>
+          <div className="experience-language">
+            <Image
+              src="/ts.png"
+              alt="Typescript Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure m2"><span>2 anos</span></div>
+            </div>
+          </div>
+          <div className="experience-language">
+            <Image
+              src="/java.png"
+              alt="Java Logo"
+              width={40}
+              height={40}
+              priority
+            />
+            <div className="experience-unit">
+              <div className="experience-measure m1"><span>1 ano</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
