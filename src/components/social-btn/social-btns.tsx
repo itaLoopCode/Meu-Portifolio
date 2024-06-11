d685aba606373d6a3f23229839a0cdf796f5ebde
itaLoopCode <italo_j.s@hotmail.com>
2024-06-11 13:51:43 -0300

    commited

--- src/components/social-btn/social-btns.tsx
+++ src/components/social-btn/social-btns.tsx
@@ -3,13 +3,13 @@ import InstaIcon from "../icons/insta-icon";
 import LinkedinIcon from "../icons/Linkedin-icon";
 import YoutubeIcon from "../icons/youtube-Icon";
 
-import '@/components/buttons/buttons.scss'
+import '@/components/social-btn/social-btns.scss'
 
 interface SocialBtnsProps {}
 export default function SocialBtns(props: SocialBtnsProps) {
   return (
     <>
-      <div className="btns">
+      <div className="social">
         <a href=""><InstaIcon /></a>
         <a href=""><LinkedinIcon /></a>
         <a href="">< GithubIcon /></a>
